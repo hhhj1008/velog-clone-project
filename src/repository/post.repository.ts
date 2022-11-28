b775a220c94d60f8b4b4ebb99b6d04033aff2cd9
ahnsucheol <josephscahn@gmail.com>
2022-11-28 13:57:14 +0900

    feature : 메인 페이지 검색 기능 query로 keyword를 받아와서 게시글의 제목, 내용, 태그이름에 해당 키워드가 포함되는 모든 게시글의 정보 반환

--- src/repository/post.repository.ts
+++ src/repository/post.repository.ts
@@ -236,4 +236,47 @@ export class PostRepository extends Repository<Post> {
   }
 
   async interestedPostList(tag_ids: string) {}
+
+  async mainSearch(keywords: string) {
+    return this.query(
+      `
+        WITH tags AS (
+          SELECT 
+            post.id post_id,
+            tag.id tag_id,
+            JSON_ARRAYAGG(
+              JSON_OBJECT(
+                'tag_id',tag.id, 
+                'tag_name', tag.name
+              )
+            ) as tags   
+            FROM post 
+            LEFT JOIN post_tag ON post.id = post_tag.post_id
+            LEFT JOIN tag ON post_tag.tag_id = tag.id
+            WHERE post_tag.post_id = post.id
+        )
+        
+        SELECT 
+            distinct
+            post.id post_id,
+            post.title,
+            post.content,
+            post.thumbnail,
+            post.comment_count,
+            DATE_FORMAT(post.create_at, '%Y년 %m월 %d일') create_at,
+            tags.tags,
+            user.id user_id,
+            user.name,
+            user.profile_image
+          FROM post
+          LEFT JOIN user ON post.user_id = user.id
+          LEFT JOIN post_tag ON post.id = post_tag.post_id
+          LEFT JOIN tag ON post_tag.tag_id = tag.id
+          LEFT JOIN tags ON tags.post_id = post.id
+          WHERE post.title REGEXP ? OR post.content REGEXP ? OR tag.name REGEXP ?
+          ORDER BY post.create_at DESC
+      `,
+      [keywords, keywords, keywords],
+    );
+  }
 }
