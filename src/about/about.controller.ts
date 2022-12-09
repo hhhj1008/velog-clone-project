dbbd3a88d8615f30186bd2c28b47af1baf7ed107
hyunjung <hhhj.1486@gmail.com>
2022-12-09 10:54:37 +0900

    fix: 소개글 API 주소 소개글 API 주소 수정

--- src/about/about.controller.ts
+++ src/about/about.controller.ts
@@ -10,14 +10,14 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 export class AboutController {
   constructor(private readonly aboutService: AboutService) {}
 
-  @Get('/:user_id/about')
+  @Get('/:user_id')
   async getAbout(@Param('user_id') user_id: number, @ValidateToken() user?: User) {
     const result = await this.aboutService.getAboutBlog(user_id, user);
 
     return { statusCode: 200, about: result };
   }
 
-  @Patch('/about')
+  @Patch('')
   @UseGuards(JwtAuthGuard)
   async editAboutBlog(@Body() data: AboutBlogDto, @GetUser() user: User) {
     const result = await this.aboutService.editAboutBlog(data.about_blog, user);
